4961acf99284e432bd6c887903ff5ff2b90fa98b
Mojashi <akihiro1001@gmail.com>
2020-06-26 12:51:07 +0900

    implement

--- src/spear.c
+++ src/spear.c
@@ -12,6 +12,16 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+  lb = 0, ub = 1000000001;
+  while(ub-lb>1){
+    int mid = (ub + lb) / 2;
+    int cou = 0;
+    for(int i = 0; n > i; i++)
+      cou += A[i] / mid;
+    if(cou >= k) lb = mid;
+    else ub = mid;
+  }
 
+  printf("%d\n", lb);
   return 0;
 }
